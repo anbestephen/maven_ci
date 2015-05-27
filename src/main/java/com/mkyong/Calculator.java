0841859cf4f67fc3acd26868e9decb192d672eed
Ambasager Debretsion <anbestephen@gmail.com>
2015-05-27 18:55:26 -0400

    implement calculator class

--- src/main/java/com/mkyong/Calculator.java
+++ src/main/java/com/mkyong/Calculator.java
@@ -6,5 +6,21 @@ package com.mkyong;
  */
 public class Calculator 
 {
+
+	public int add(int i, int j) {
+		return i+j;
+	}
+
+	public int subtract(int i, int j) {
+		return i-j;
+	}
+
+	public int multiply(int i, int j) {
+		return i*j;
+	}
+
+	public int divide(int i, int j) {
+		return i/j;
+	}
    
 }
--- src/test/java/com/mkyong/CalculatorTest.java
+++ src/test/java/com/mkyong/CalculatorTest.java
@@ -1,5 +1,38 @@
 package com.mkyong;
 
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
 public class CalculatorTest{
+	
+	Calculator calc;
+	
+	@Before
+	public void setUp(){
+		calc=new Calculator();
+	}
+	
+	@Test
+	public void testAdd(){
+		
+		assertEquals(3, calc.add(1,2));
+	}
+	
+	@Test
+	public void testSubtract(){
+		assertEquals(5, calc.subtract(8,3));
+	}
+	
+	@Test
+	public void testMultiply(){
+		assertEquals(15, calc.multiply(3,5));
+	}
+	
+	@Test
+	public void testDivide(){
+		assertEquals(3, calc.divide(12,4));
+	}
+	
 	
 }
