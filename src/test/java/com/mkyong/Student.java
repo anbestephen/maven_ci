af73f359d2eaa680128dbe210eb7f15e72b46a9a
Ambasager Debretsion <anbestephen@gmail.com>
2015-05-27 19:31:59 -0400

    added student class

---
+++ src/test/java/com/mkyong/Student.java
@@ -0,0 +1,41 @@
+package com.mkyong;
+
+public class Student {
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getGpa() {
+		return gpa;
+	}
+
+	public void setGpa(double gpa) {
+		this.gpa = gpa;
+	}
+
+	private String name;
+	private int age;
+	private double gpa;
+}
